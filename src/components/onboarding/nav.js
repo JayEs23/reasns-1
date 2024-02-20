ae448c69a4074ce2b1edf8c907704af3674f5631
minealone <3zpy20xojymopi2581x9mc31rdyefj@bots.bitbucket.org>
2024-02-20 15:30:18 +0100

    updated

--- src/components/onboarding/nav.js
+++ src/components/onboarding/nav.js
@@ -1,7 +1,16 @@
-export default function nav() {
+import React from "react";
+
+export default function Nav() {
   return (
     <nav>
-        
+      <ul className="navbar">
+        <li><a href=""></a></li>
+        <div className="mainNav">
+            <li><a href="">Home</a></li>
+            <li><a href="">Profiles</a></li>
+            <li><a href="">Log in/Sign up</a></li>
+        </div>
+    </ul>
     </nav>
-  )
+  );
 }
